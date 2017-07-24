36048d99e156cdf329d2e92ae73dae3fdb7e955a
Edwin Moses <workockmoses@gmail.com>
2017-07-24 11:19:30 +0300

    added functional middlewares

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+//View Middleware
 app.set('view engine', 'ejs');
-app.set('views', '/views');
+app.set('views', './views');
+
+//Static file Middleware
+app.use(express.static('public'));
+
+//Body parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 
 app.listen(port, '127.0.0.1', () => {
