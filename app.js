fc9559c3b39281312349e3feea5aae77c30ff4b7
Edwin Moses <workockmoses@gmail.com>
2017-07-24 11:06:23 +0300

    set view engine

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', '/views');
+
 
 app.listen(port, '127.0.0.1', () => {
   console.log("Server running locally");
